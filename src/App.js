0632ec9573dd1c8b1571cde6a55448f3968def7c
Matthew Hayes Jacobs <matthewhayesjacobs@Matthews-MBP-2.lan>
2022-07-21 18:49:26 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import axios from 'axios';
 
 const App = () => {
   const [apiData, setApiData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
-      const response = await await axios.get('https://jsonplaceholder.typicode.com/photos')
+      const response = await axios.get('https://jsonplaceholder.typicode.com/photos')
       setApiData(response.data);
+      setLoading(false);
     }
 
     getData();
@@ -18,14 +20,16 @@ const App = () => {
   return (
     <div className="App">
       <div id="container">
-      {apiData.map((item) => {
-        return (
-          <div>
-            <h1><a href={item.url}>{item.title}</a></h1>
-            <img src={item.thumbnailUrl} />
-          </div>
-        )
-      })}
+        {loading && <div id="loading-container"><span id="loading">Loading...</span></div>}
+        {apiData.map((item) => {
+          return (
+            <div className="item">
+              <h1><a href={item.url}>{item.title}</a></h1>
+              <img src={item.thumbnailUrl} />
+              <hr />
+            </div>
+          )
+        })}
       </div>
     </div>
   );
